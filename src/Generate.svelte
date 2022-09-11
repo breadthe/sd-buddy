ba9813ebadf8c90e89f08e0e842155cf736dd27c
breadthe <breadthe@users.noreply.github.com>
2022-09-11 10:51:13 -0500

    Fix random seed

--- src/Generate.svelte
+++ src/Generate.svelte
@@ -1,143 +1,166 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/tauri";
-  import { v4 as uuidv4 } from "uuid";
-  import { AlertTypes, Rating } from "./types";
-  import type { Run } from "./types";
-  import { runs, reusePrompt, stableDiffusionDirectory } from "./store";
-  import Alert from "./lib/Alert.svelte";
-  import RunItem from "./lib/RunItem.svelte";
+  import { invoke } from "@tauri-apps/api/tauri"
+  import { v4 as uuidv4 } from "uuid"
+  import { AlertTypes, Rating } from "./types"
+  import type { Run } from "./types"
+  import { runs, reusePrompt, stableDiffusionDirectory } from "./store"
+  import Alert from "./lib/Alert.svelte"
+  import RunItem from "./lib/RunItem.svelte"
 
-  let stableDiffusionOutputDirectory: string = "";
-  let stableDiffusionCommand: string = "";
-  let stableDiffusionCommandHtml: string = "";
-  let rustResponse: string = "";
-  let rustError: string = "";
+  let stableDiffusionOutputDirectory: string = ""
+  let stableDiffusionCommand: string = ""
+  let stableDiffusionCommandHtml: string = ""
+  let rustResponse: string = ""
+  let rustError: string = ""
 
   // Flags
-  let isGenerating: boolean = false;
-  let useCustomSteps: boolean = false;
+  let isGenerating: boolean = false
+  let useCustomSteps: boolean = false
 
-  // Form parameters
-  const placeholder =
-    "a red juicy apple floating in outer space, like a planet";
-  let prompt: string = "";
-  let defaultSteps: number = 10; // --ddim_steps, default 50 (keeping it 10 because slow computer)
-  let steps: number = defaultSteps; // selected steps
-  let maxSteps: number = 100;
-  let stepsOptions: number[] = [1, 2, 3, 4, 5, 10, 15, 25, 50, 75, 100];
-  let defaultScale: number = 8; // --scale, default 7.5 but we round to 8
-  let scale: number = defaultScale; // selected scale (context free guidance)
-  let maxScale: number = 20; // 1 is AI almost ignores prompt, 20 is "stick to my prompt"
-  let defaultIter: number = 1; // --n_iter, default 1
-  let iter: number = defaultIter; // selected iter(ations?)
-  let maxIter: number = 10; // no idea what is should be, go with 10 for now
-  let defaultSamples: number = 1; // --n_samples, default 3
-  let samples: number = defaultSamples; // selected samples
-  let maxSamples: number = 10; // no idea what is should be, go with 10 for now
-  let defaultHeight: number = 512; // --H default 512
-  let height: number = defaultHeight; // selected height
-  let defaultWidth: number = 512; // --W default 512
-  let width: number = defaultWidth; // selected width
-  let defaultSeed: number = 42; // --seed default 42, set to -1 for random
-  let seed: number = defaultSeed; // selected seed
-  let maxSeed: number = 4294967295;
-  let randomSeed: boolean = false;
+  // Form parameters:
+
+  // prompt
+  const placeholder = "a red juicy apple floating in outer space, like a planet"
+  let prompt: string = ""
+
+  // --ddim_steps
+  let defaultSteps: number = 10 // --ddim_steps, default 50 (keeping it 10 because slow computer)
+  let steps: number = defaultSteps // selected steps
+  let maxSteps: number = 100
+  let stepsOptions: number[] = [1, 2, 3, 4, 5, 10, 15, 25, 50, 75, 100]
+
+  // --scale
+  let defaultScale: number = 8 // --scale, default 7.5 but we round to 8
+  let scale: number = defaultScale // selected scale (context free guidance)
+  let maxScale: number = 20 // 1 is AI almost ignores prompt, 20 is "stick to my prompt"
+
+  // --n_iter
+  let defaultIter: number = 1 // --n_iter, default 1
+  let iter: number = defaultIter // selected iter(ations?)
+  let maxIter: number = 10 // no idea what it should be, go with 10 for now
+
+  // --n_samples
+  let defaultSamples: number = 1 // --n_samples, default 3
+  let samples: number = defaultSamples // selected samples
+  let maxSamples: number = 10 // no idea what it should be, go with 10 for now
+
+  // --H
+  let defaultHeight: number = 512 // --H default 512
+  let height: number = defaultHeight // selected height
+
+  // --W
+  let defaultWidth: number = 512 // --W default 512
+  let width: number = defaultWidth // selected width
+
+  // --seed
+  let defaultSeed: number = 42 // --seed default 42, set to -1 for random
+  let seed: number = defaultSeed // selected seed
+  let maxSeed: number = 4294967295
+  let useRandomSeed: boolean = false
+  // use the initial random seed when Random is checked for the first run, regenerate for subsequent runs
+  $: if (useRandomSeed || (useRandomSeed && isGenerating && currentCopy > 1)) {
+    seed = getRandomSeed()
+  }
 
   // parameters for generating multiple images with the same settings
-  let copiesOptions: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  let defaultCopies: number = 1; // number of copies to generate
-  let copies: number;
-  let currentCopy: number = 1;
+  let copiesOptions: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  let defaultCopies: number = 1 // number of copies to generate
+  let copies: number
+  let currentCopy: number = 1
 
   // Duration timers
-  let elapsed: number = 0; // in ms
-  $: elapsedSeconds = new Intl.NumberFormat("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 2, style: "unit", unit: "second" }).format(elapsed / 1000);
+  let elapsed: number = 0 // in ms
+  $: elapsedSeconds = new Intl.NumberFormat("en-US", {
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2,
+    style: "unit",
+    unit: "second",
+  }).format(elapsed / 1000)
 
-  let currentRun: Run; // the currently generated run
+  let currentRun: Run // the currently generated run
 
-  $: numPromptTokens = Math.ceil(prompt.length / 4); // very rough estimation https://www.reddit.com/r/StableDiffusion/comments/wl4cn3/the_maximum_usable_length_of_a_stable_diffusion/
+  $: numPromptTokens = Math.ceil(prompt.length / 4) // very rough estimation https://www.reddit.com/r/StableDiffusion/comments/wl4cn3/the_maximum_usable_length_of_a_stable_diffusion/
 
   $: {
-    stableDiffusionCommand = `python scripts/txt2img.py --prompt "${prompt}" --plms --n_samples ${samples?.toString()} --scale ${scale?.toString()} --n_iter ${iter?.toString()} --ddim_steps ${steps?.toString()} --H ${height?.toString()} --W ${width?.toString()} --seed ${seed?.toString()}`;
-    stableDiffusionCommandHtml = `python scripts/txt2img.py --prompt <strong>"${prompt}"</strong> --plms --n_samples <strong>${samples}</strong> --scale <strong>${scale}</strong> --n_iter <strong>${iter}</strong> --ddim_steps <strong>${steps}</strong> --H <strong>${height}</strong> --W <strong>${width}</strong>` + (randomSeed ? '' : ` --seed <strong>${seed}</strong>`);
+    stableDiffusionCommand = `python scripts/txt2img.py --prompt "${prompt}" --plms --n_samples ${samples?.toString()} --scale ${scale?.toString()} --n_iter ${iter?.toString()} --ddim_steps ${steps?.toString()} --H ${height?.toString()} --W ${width?.toString()} --seed ${seed?.toString()}`
+    stableDiffusionCommandHtml = `python scripts/txt2img.py --prompt <strong>"${prompt}"</strong> --plms --n_samples <strong>${samples}</strong> --scale <strong>${scale}</strong> --n_iter <strong>${iter}</strong> --ddim_steps <strong>${steps}</strong> --H <strong>${height}</strong> --W <strong>${width}</strong> --seed <strong>${seed}</strong>`
   }
 
   $: {
     if ($stableDiffusionDirectory) {
-      stableDiffusionOutputDirectory = `${$stableDiffusionDirectory}/outputs/txt2img-samples`;
+      stableDiffusionOutputDirectory = `${$stableDiffusionDirectory}/outputs/txt2img-samples`
     }
   }
 
   $: if (Object.keys($reusePrompt).length) {
-    prompt = $reusePrompt?.prompt ?? prompt;
-    steps = $reusePrompt?.steps ?? steps;
-    iter = $reusePrompt?.iter ?? iter;
-    samples = $reusePrompt?.samples ?? samples;
-    scale = $reusePrompt?.scale ?? scale;
-    seed = $reusePrompt?.seed ?? seed;
-    height = $reusePrompt?.height ?? height;
-    width = $reusePrompt?.width ?? width;
+    prompt = $reusePrompt?.prompt ?? prompt
+    steps = $reusePrompt?.steps ?? steps
+    iter = $reusePrompt?.iter ?? iter
+    samples = $reusePrompt?.samples ?? samples
+    scale = $reusePrompt?.scale ?? scale
+    seed = $reusePrompt?.seed ?? seed
+    height = $reusePrompt?.height ?? height
+    width = $reusePrompt?.width ?? width
 
     // Reset the prompt so we can modify the values.
-    $reusePrompt = {} as Run;
+    $reusePrompt = {} as Run
   }
 
   async function saveRun(run: Run) {
-    runs.push(run);
+    runs.push(run)
   }
 
   // resets the form to its original state and clears the response alerts
   function resetForm() {
-    reusePrompt.set(<Run>{});
-    prompt = "";
-    steps = defaultSteps;
-    samples = defaultSamples;
-    scale = defaultScale;
-    iter = defaultIter;
-    scale = defaultScale;
-    height = defaultHeight;
-    width = defaultWidth;
-    seed = defaultSeed;
-    isGenerating = false;
-    useCustomSteps = false;
-    elapsed = 0;
-    rustResponse = "";
-    rustError = "";
-    currentRun = null;
+    reusePrompt.set(<Run>{})
+    prompt = ""
+    steps = defaultSteps
+    samples = defaultSamples
+    scale = defaultScale
+    iter = defaultIter
+    scale = defaultScale
+    height = defaultHeight
+    width = defaultWidth
+    seed = defaultSeed
+    useRandomSeed = false
+    copies = defaultCopies
+    isGenerating = false
+    useCustomSteps = false
+    elapsed = 0
+    rustResponse = ""
+    rustError = ""
+    currentRun = null
   }
 
   // queue up multiple runs
   async function generate() {
     for (let i = 1; i <= copies; i++) {
-      currentCopy = i;
-      console.log(`Queueing ${currentCopy}`);
-      const result = await doTheWork(currentCopy);
-      console.log(result);
+      currentCopy = i
+      console.log(`Queueing ${currentCopy}`)
+      const result = await doTheWork(currentCopy)
+      console.log(result)
     }
   }
 
+  // generate a random seed between 0 and maxSeed
+  const getRandomSeed = () => Math.floor(Math.random() * maxSeed)
+
   // generate a single run
   async function doTheWork(n: number) {
-    if ($stableDiffusionDirectory.trim() === "") return;
-    if (stableDiffusionCommand.trim() === "") return;
+    if ($stableDiffusionDirectory.trim() === "") return
+    if (stableDiffusionCommand.trim() === "") return
 
-    isGenerating = true;
-    elapsed = 0;
-    rustResponse = "";
-    rustError = "";
-    currentRun = null;
+    isGenerating = true
+    elapsed = 0
+    rustResponse = ""
+    rustError = ""
+    currentRun = null
 
     // timer that runs every 100ms
-    const startTimer = new Date();
+    const startTimer = new Date()
     const timer = setInterval(() => {
-      elapsed = Math.floor(new Date().getTime() - startTimer.getTime());
-    }, 100);
-
-    // generate a random seed between 0 and maxSeed
-    let tmpSeed = seed
-    if (randomSeed) {
-        tmpSeed = Math.floor(Math.random() * maxSeed)
-    }
+      elapsed = Math.floor(new Date().getTime() - startTimer.getTime())
+    }, 100)
 
     const run: Run = {
       id: uuidv4(),
@@ -148,10 +171,10 @@
       iter,
       height,
       width,
-      seed: tmpSeed,
+      seed,
       started_at: startTimer,
       rating: Rating.One,
-    };
+    }
 
     // Invoke the Stable Diffusion command
     await invoke("stable_diffusion_command", {
@@ -159,37 +182,37 @@
       command: stableDiffusionCommand,
     })
       .then(async (res) => {
-        rustResponse = JSON.stringify(res);
+        rustResponse = JSON.stringify(res)
 
-        run.image_name = "image.png"; // @todo use magic to get the image name
+        run.image_name = "image.png" // @todo use magic to get the image name
       })
       .catch((err) => {
-        rustError = JSON.stringify(err);
+        rustError = JSON.stringify(err)
       })
       .finally(() => {
-        isGenerating = false;
+        isGenerating = false
 
-        clearInterval(timer);
+        clearInterval(timer)
 
-        run.ended_at = new Date();
-        run.elapsed = elapsed; // in s
-      });
+        run.ended_at = new Date()
+        run.elapsed = elapsed // in s
+      })
 
     await getLatestImageFromOutputDirectory(elapsed).then((image_name) => {
-      run.image_name = JSON.parse(image_name);
-    });
+      run.image_name = JSON.parse(image_name)
+    })
 
-    saveRun(run);
+    saveRun(run)
 
-    currentRun = run;
+    currentRun = run
 
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve(`done ${n}`);
+        resolve(`done ${n}`)
 
-        if (rustError) reject(rustError);
-      }, 10);
-    });
+        if (rustError) reject(rustError)
+      }, 10)
+    })
   }
 
   // Get the latest image from the output directory
@@ -197,28 +220,28 @@
   async function getLatestImageFromOutputDirectory(
     elapsed: number
   ): Promise<string> {
-    let latest_image = "";
-    let seconds = Math.ceil(elapsed / 1000) + 10; // convert ms to seconds, add 10 seconds to be safe
+    let latest_image = ""
+    let seconds = Math.ceil(elapsed / 1000) + 10 // convert ms to seconds, add 10 seconds to be safe
 
     await invoke("get_latest_image", {
       dirPath: stableDiffusionOutputDirectory,
       elapsed: seconds.toString(),
     })
       .then((res) => {
-        latest_image = JSON.stringify(res);
+        latest_image = JSON.stringify(res)
       })
       .catch((err) => {
-        console.log(err);
+        console.log(err)
 
-        latest_image = "";
-      });
+        latest_image = ""
+      })
 
-    return latest_image;
+    return latest_image
   }
 
   function handleCustomSteps(event: Event) {
     // very hacky way of swapping input <-> select without losing the value
-    useCustomSteps = event.target.selectedOptions[0].innerText === "custom";
+    useCustomSteps = event.target.selectedOptions[0].innerText === "custom"
   }
 </script>
 
@@ -263,8 +286,8 @@
             <button
               class="transparent"
               on:click={() => {
-                useCustomSteps = false;
-                steps = defaultSteps;
+                useCustomSteps = false
+                steps = defaultSteps
               }}
               title="Switch to dropdown"
             >
@@ -340,17 +363,28 @@
       {/if}
 
       <div class="flex items-center justify-between gap-4">
-          <label class="flex-1 flex flex-col">
-            <span class="font-bold">Seed</span>
+        <label class="flex-1 flex flex-col">
+          <span class="font-bold">Seed</span>
 
-            <input type="number" bind:value={seed} disabled={randomSeed} class:opacity-30={randomSeed} min="-1" max={maxSeed} />
-          </label>
+          <input
+            type="number"
+            bind:value={seed}
+            disabled={useRandomSeed}
+            class:opacity-30={useRandomSeed}
+            min="-1"
+            max={maxSeed}
+          />
+        </label>
 
-          <label class="flex flex-col text-right">
-            <span class="font-bold">Random</span>
+        <label class="flex flex-col text-right">
+          <span class="font-bold">Random</span>
 
-            <input type="checkbox" bind:checked={randomSeed} class="my-2 border" />
-          </label>
+          <input
+            type="checkbox"
+            bind:checked={useRandomSeed}
+            class="my-2 border"
+          />
+        </label>
       </div>
     </div>
 
