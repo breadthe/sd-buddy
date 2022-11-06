8fb8f9f2516b72cfc3f1ffed4bd3e8410562d1c7
breadthe <breadthe@users.noreply.github.com>
2022-11-05 20:48:42 -0500

    Extract Random Seed form input & persist in store

--- src/lib/Generate.svelte
+++ src/lib/Generate.svelte
@@ -55,7 +55,8 @@
   import Samples from "./form/Samples.svelte"
   import Height from "./form/Height.svelte"
   import Width from "./form/Width.svelte"
-    import Seed from "./form/Seed.svelte"
+  import Seed from "./form/Seed.svelte"
+  import UseRandomSeed from "./form/UseRandomSeed.svelte"
 
   let stableDiffusionOutputDirectory: string = ""
   let stableDiffusionCommand: string = ""
@@ -347,19 +348,7 @@
       <div class="flex items-center justify-between gap-4">
         <Seed />
 
-        <!-- <label class="flex flex-col text-right w-full"> -->
-        <div class="flex flex-col text-right w-full">
-          <div class="flex items-center gap-2">
-            <label for="randomSeed" class="font-bold">Random</label>
-
-            <HelpBubble
-              title={`Using seed=-1 for a random seed does not tell you what the value is. If you want to have a record of what seed was used, check this option to get a random seed between 1-${$maxSeed}.`}
-            />
-          </div>
-
-          <input id="randomSeed" type="checkbox" bind:checked={$useRandomSeed} class="my-2 border" />
-        </div>
-        <!-- </label> -->
+        <UseRandomSeed />
       </div>
     </div>
 
---
+++ src/lib/form/UseRandomSeed.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  // store imports
+  import { maxSeed, useRandomSeed } from "../../store"
+
+  // component imports
+  import HelpBubble from "../HelpBubble.svelte"
+</script>
+
+<div class="flex flex-col text-right w-full">
+  <div class="flex items-center gap-2">
+    <label for="randomSeed" class="font-bold">Random Seed</label>
+
+    <HelpBubble
+      title={`Using seed=-1 for a random seed does not tell you what the value is. If you want to have a record of what seed was used, check this option to get a random seed between 1-${$maxSeed}.`}
+    />
+  </div>
+
+  <input id="randomSeed" type="checkbox" bind:checked={$useRandomSeed} class="my-2 border" />
+</div>
+
+<style>
+</style>
